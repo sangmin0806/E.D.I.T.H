d823d6fc375d15e9334b0938386c3c0d17a04ea9
sangmin0806 <cjstkd2866@naver.com>
2024-10-30 14:22:39 +0900

    feat:사용자 gitlab personal access token AES 알고리즘으로 암호화

--- edith-back/user/src/main/java/com/ssafy/edith/user/api/service/UserService.java
+++ edith-back/user/src/main/java/com/ssafy/edith/user/api/service/UserService.java
@@ -3,6 +3,7 @@ package com.ssafy.edith.user.api.service;
 import com.ssafy.edith.user.api.request.UserRequest;
 import com.ssafy.edith.user.entity.User;
 import com.ssafy.edith.user.repository.UserRepository;
+import com.ssafy.edith.user.util.EncryptionUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -13,6 +14,7 @@ public class UserService {
 
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder passwordEncoder;
+    private final EncryptionUtil encryptionUtil;
 
     public User signUp(UserRequest userRequest) {
 
@@ -21,12 +23,13 @@ public class UserService {
         }
 
         String encryptedPassword = passwordEncoder.encode(userRequest.password());
+        String encryptedAccessToken = encryptionUtil.encrypt(userRequest.gitlabPersonalAccessToken());
 
         User user = User.builder()
                 .email(userRequest.email())
                 .password(encryptedPassword)
                 .gitlabBaseUrl(userRequest.gitlabBaseUrl())
-                .gitlabPersonalAccessToken(userRequest.gitlabPersonalAccessToken())
+                .gitlabPersonalAccessToken(encryptedAccessToken)
                 .build();
 
         return userRepository.save(user);
