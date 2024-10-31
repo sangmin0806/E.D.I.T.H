eaec0ead4263fb612ed85dda5cbd8fcaf4ecece8
유재광 <marmong9770@gmail.com>
2024-10-31 10:55:12 +0900

    init project

---
+++ edith-back/developmentassistant/src/main/java/com/edith/developmentassistant/DevelopmentassistantApplication.java
@@ -0,0 +1,13 @@
+package com.edith.developmentassistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevelopmentassistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevelopmentassistantApplication.class, args);
+	}
+
+}
---
+++ edith-back/developmentassistant/src/test/java/com/edith/developmentassistant/DevelopmentassistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edith.developmentassistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevelopmentassistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
