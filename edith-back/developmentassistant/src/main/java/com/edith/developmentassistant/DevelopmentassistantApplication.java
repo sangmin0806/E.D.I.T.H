61813cd3a2f4998e00d5fbe6c9ddd7ea9c174909
유재광 <marmong9770@gmail.com>
2024-11-01 13:53:50 +0900

    fix: 등록 API 에 userId 추가

--- edith-back/developmentassistant/src/main/java/com/edith/developmentassistant/DevelopmentassistantApplication.java
+++ edith-back/developmentassistant/src/main/java/com/edith/developmentassistant/DevelopmentassistantApplication.java
@@ -2,9 +2,11 @@ package com.edith.developmentassistant;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class DevelopmentassistantApplication {
 
 	public static void main(String[] args) {
