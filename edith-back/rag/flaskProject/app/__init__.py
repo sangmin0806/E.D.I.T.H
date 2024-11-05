ee61b9f6552e6a06f52602b1b3445cde5799ec71
Lee-JoungHyun <doublehyun98@gmail.com>
2024-11-05 14:25:53 +0900

    feat: 코드리뷰 청크화, 포트폴리오 참고 자료 추가 반환

--- edith-back/rag/flaskProject/app/__init__.py
+++ edith-back/rag/flaskProject/app/__init__.py
@@ -9,6 +9,7 @@ load_dotenv()  # ...env 파일에서 환경 변수 로드
 def create_app():
     app = Flask(__name__)
     app.config['OPENAI_API_KEY'] = os.getenv('OPENAI_API_KEY')
+    app.config['MAX_TOKEN_LENGTH'] = os.getenv('MAX_TOKEN_LENGTH')
 
     from .routes import routes_bp
     app.register_blueprint(routes_bp)
