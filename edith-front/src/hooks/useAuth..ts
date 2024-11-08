60334c56a2c7efe92f5cf7d030972af6ece1dc98
신민경 <tlsalsrud252@naver.com>
2024-11-08 12:06:39 +0900

    feat: 404 NotFoundPage 구현

--- edith-front/src/hooks/useAuth..ts
+++ edith-front/src/hooks/useAuth..ts
@@ -5,38 +5,38 @@ import { useEffect } from "react";
 export const useRedirectIfLoggedIn = async (
   redirectTo: string = "/dashboard"
 ) => {
-  const navigate = useNavigate();
-  try {
-    const result = await validAccessTokenRequest();
-    useEffect(() => {
-      const checkLogin = async () => {
-        if (result) {
-          alert("잘못된 접근입니다.");
-          navigate(redirectTo);
-        }
-      };
-      checkLogin();
-    }, [navigate, redirectTo]);
-  } catch {
-    alert("서버 에러가 발생하였습니다.");
-    navigate("/");
-  }
+  //   const navigate = useNavigate();
+  //   try {
+  //     const result = await validAccessTokenRequest();
+  //     useEffect(() => {
+  //       const checkLogin = async () => {
+  //         if (result) {
+  //           alert("잘못된 접근입니다.");
+  //           navigate(redirectTo);
+  //         }
+  //       };
+  //       checkLogin();
+  //     }, [navigate, redirectTo]);
+  //   } catch {
+  //     alert("서버 에러가 발생하였습니다.");
+  //     navigate("/");
+  //   }
 };
 export const useRedirectIfNotLoggedIn = async (redirectTo: string = "/") => {
-  const navigate = useNavigate();
-  try {
-    const result = await validAccessTokenRequest();
-    useEffect(() => {
-      const checkLogin = async () => {
-        if (result) {
-          alert("잘못된 접근입니다.");
-          navigate(redirectTo);
-        }
-      };
-      checkLogin();
-    }, [navigate, redirectTo]);
-  } catch {
-    alert("서버 에러가 발생하였습니다.");
-    navigate("/");
-  }
+  //   const navigate = useNavigate();
+  //   try {
+  //     const result = await validAccessTokenRequest();
+  //     useEffect(() => {
+  //       const checkLogin = async () => {
+  //         if (result) {
+  //           alert("잘못된 접근입니다.");
+  //           navigate(redirectTo);
+  //         }
+  //       };
+  //       checkLogin();
+  //     }, [navigate, redirectTo]);
+  //   } catch {
+  //     alert("서버 에러가 발생하였습니다.");
+  //     navigate("/");
+  //   }
 };
