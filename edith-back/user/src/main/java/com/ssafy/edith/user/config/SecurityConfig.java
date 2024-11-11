68f1cf5b06257a313d2b751402e58999123cabf1
sangmin0806 <cjstkd2866@naver.com>
2024-11-11 15:21:33 +0900

    fix:scg cookie 설정 수정

--- edith-back/user/src/main/java/com/ssafy/edith/user/config/SecurityConfig.java
+++ edith-back/user/src/main/java/com/ssafy/edith/user/config/SecurityConfig.java
@@ -23,7 +23,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                .cors(cors -> cors.disable())
                 .csrf(AbstractHttpConfigurer::disable)
                 .httpBasic(AbstractHttpConfigurer::disable)
                 .formLogin(AbstractHttpConfigurer::disable)
