f8b877dcca9b3c5b1efe678c9f35d1642be602ef
sangmin0806 <cjstkd2866@naver.com>
2024-11-11 15:01:18 +0900

    fix:security 해결

--- edith-back/user/src/main/java/com/ssafy/edith/user/config/SecurityConfig.java
+++ edith-back/user/src/main/java/com/ssafy/edith/user/config/SecurityConfig.java
@@ -36,9 +36,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(Arrays.asList("*"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173","http://k11c206.p.ssafy.io"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
