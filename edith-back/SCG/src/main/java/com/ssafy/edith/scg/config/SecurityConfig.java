ab62e890b4fbc2253fd1d41babec165f6d61b333
sangmin0806 <cjstkd2866@naver.com>
2024-11-11 14:26:57 +0900

    fix: user서버 CORS 오류

--- edith-back/SCG/src/main/java/com/ssafy/edith/scg/config/SecurityConfig.java
+++ edith-back/SCG/src/main/java/com/ssafy/edith/scg/config/SecurityConfig.java
@@ -30,7 +30,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOriginPatterns(Arrays.asList("*"));
+        config.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
         config.setAllowedHeaders(Arrays.asList("*"));
         config.setAllowCredentials(true);
