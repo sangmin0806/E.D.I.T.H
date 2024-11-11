9da0930cf6a1eb596743d113758caf03320c84e9
Lee-JoungHyun <doublehyun98@gmail.com>
2024-11-11 15:26:57 +0900

    feat: 저장된 포트폴리오 단일 조회

--- edith-back/developmentassistant/src/main/java/com/edith/developmentassistant/service/dto/PortfolioDto.java
+++ edith-back/developmentassistant/src/main/java/com/edith/developmentassistant/service/dto/PortfolioDto.java
@@ -4,11 +4,12 @@ import com.edith.developmentassistant.domain.Portfolio;
 import com.edith.developmentassistant.domain.UserProject;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.format.DateTimeFormatter;
 
 @Data
-@Builder
+@NoArgsConstructor
 public class PortfolioDto {
     private String name;
     private Long projectId;
@@ -24,5 +25,15 @@ public class PortfolioDto {
         this.startDate = portfolio.getStartDate().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));;
         this.endDate = portfolio.getEndDate().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));;
         this.portfolio = portfolio.getContent();
+    }
+
+    @Builder
+    public PortfolioDto(String name, Long projectId, String content, String startDate, String endDate, String portfolio) {
+        this.name = name;
+        this.projectId = projectId;
+        this.content = content;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.portfolio = portfolio;
     }
 }
