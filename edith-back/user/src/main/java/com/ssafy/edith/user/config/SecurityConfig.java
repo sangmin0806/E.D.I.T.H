f01d163877caabcecf3c8d03443903e8a3cca0e9
sangmin0806 <cjstkd2866@naver.com>
2024-11-11 17:37:03 +0900

    feat: develop assistant db연결

--- edith-back/user/src/main/java/com/ssafy/edith/user/config/SecurityConfig.java
+++ edith-back/user/src/main/java/com/ssafy/edith/user/config/SecurityConfig.java
@@ -6,11 +6,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.Arrays;
 
 @Configuration
 public class SecurityConfig {
