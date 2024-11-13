ac9746a0506bab836cc75dceec96ea8b789e5568
sangmin0806 <cjstkd2866@naver.com>
2024-11-13 17:55:18 +0900

    fix: Qdrant data 형식수정

--- edith-back/user/src/main/java/com/ssafy/edith/user/client/valueobject/FaceEmbeddingRegisterRequest.java
+++ edith-back/user/src/main/java/com/ssafy/edith/user/client/valueobject/FaceEmbeddingRegisterRequest.java
@@ -2,6 +2,6 @@ package com.ssafy.edith.user.client.valueobject;
 
 public record FaceEmbeddingRegisterRequest(
         Long userId,
-        float[][] embeddingVector) {
+        float[][] embeddingVectors) {
 
 }
