6b7277a06e096f13230bb184c5c4964394c3113c
Lee-JoungHyun <doublehyun98@gmail.com>
2024-11-13 16:38:21 +0900

    fix: 프로젝트 등록시 discription 변수명 contents 로 변경

--- edith-back/rag/flaskProject/app/services/portfolio.py
+++ edith-back/rag/flaskProject/app/services/portfolio.py
@@ -1,27 +1,23 @@
 ### Portfolio 생성 로직
-
+import uuid
 # 1. 특정 Branch 의 MR 기록을 모두 가져옴 -> X
 # 2. MR ID 별로 반복
-	# 2-1. 요약 존재 시 memory 에 SAVE
-	# 2-2. 없을 경우 LLM 에 질의 -> MR 요약
+# 2-1. 요약 존재 시 memory 에 SAVE
+# 2-2. 없을 경우 LLM 에 질의 -> MR 요약
 # 3. 해당 Memory (질의 내역) 으로 최종 Portfolio 생성 -> 반환
 from langchain.memory import ConversationBufferMemory
-from app.services.llm_model import LLMModel
-from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
-import uuid
-import time
+from langchain_core.prompts import ChatPromptTemplate
+from app.services.llm_model import LLMModel
 
 # 해당 MR 요약 반환 함수, -> Memory 에 저장만
 def get_summary(llm, memory, merge_requests, summaries_dict) -> None:
-    print(merge_requests)
     summary_prompt = ChatPromptTemplate.from_template("""
             너는 프로젝트 포트폴리오 제작 전문가야
             이는 MR의 파일별 git diff 야 
             포트폴리오 제작할 요약본을 만들어줘 이때 개발자 별로 구현한 내용을 정리해줘
             
             개발자: {user_id}
-            파일 경로: {file_path}
             ===git diff===
             {diff}
 
@@ -32,11 +28,9 @@ def get_summary(llm, memory, merge_requests, summaries_dict) -> None:
 
     summary_chain = summary_prompt | llm | StrOutputParser()
     for merge_request in merge_requests:
-        start_time = time.time()
         mr_id = merge_request['mrId']
         user_id = merge_request['userId']
         diff = merge_request['diff']
-        file_path = merge_request['filePath']
 
         if summaries_dict and str(mr_id) in summaries_dict:
             # 요약본 존재 시 
@@ -48,15 +42,12 @@ def get_summary(llm, memory, merge_requests, summaries_dict) -> None:
             # LLM 에 요약
             result = summary_chain.invoke({
                 "user_id": user_id,
-                "file_path": file_path,
                 "diff": diff
             })
             memory.save_context(
                 {"input": f"{mr_id}_{user_id}"},
                 {"output": result},
             )
-        end_time = time.time()
-        print(f"포폴 요약 1회 시간 {end_time - start_time}")
     return None
 
 # 최종 Portfolio 반환 함수
@@ -64,7 +55,7 @@ def get_portfolio(llm, memory, user_id, memory_key) -> str:
     portfolio_prompt = ChatPromptTemplate.from_template("""
                 너는 프로젝트 포트폴리오 제작 전문가야
                 해당 MR 요약본으로 개인화된 포트폴리오를 제작해줘
-                (평가 하지 말고 .md 형식으로 이모티콘까지 넣어 완성해줘)
+                (HTML 형식으로 이모티콘까지 넣어 완성해줘)
 
                 개발자: {user_id}
                 
@@ -82,7 +73,7 @@ def get_portfolio(llm, memory, user_id, memory_key) -> str:
     result = portfolio_chain.invoke({
         'user_id': user_id,
         'history': memory.load_memory_variables({})[f'{memory_key}']
-    }).replace('```markdown', '').replace('```', '')
+    })
 
     return result
 
@@ -105,11 +96,10 @@ def make_portfolio(user_id, summaries, merge_requests) -> str:
     llm_model = LLMModel()
     llm = llm_model.llm
     try:
-        start_time = time.time()
         # 3. mr 반복 하며 portfolio 생성
         get_summary(llm, portfolio_memory, merge_requests, summaries_dict)
         # 4. 해당 기록 기반 최종 portfolio 생성
-        result = get_portfolio(llm, portfolio_memory, user_id, f"portfolio_{uuid}")
+        result = get_portfolio(llm, portfolio_memory, user_id, f"portfolio_{uuid}").replace('```html', '').replace('```', '').replace('\n', '')
 
         return result
 
