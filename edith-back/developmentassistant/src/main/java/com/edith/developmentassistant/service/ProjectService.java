e439abdfb7e24fbda69062a9ae4816423cdf4974
sangmin0806 <cjstkd2866@naver.com>
2024-11-15 14:03:48 +0900

    fix:test완료

--- edith-back/developmentassistant/src/main/java/com/edith/developmentassistant/service/ProjectService.java
+++ edith-back/developmentassistant/src/main/java/com/edith/developmentassistant/service/ProjectService.java
@@ -56,13 +56,9 @@ public class ProjectService {
 
     public List<ProjectResponse> getProjects(String token) {
 
-        Long userid = getUserIdByToken(token);
-        
-        for (int i = 0; i < token.length()/10; i++) {
-            System.out.println("hi");
-        }
+        Long userId = getUserIdByToken(token);
 
-        List<UserProject> userProjects = getUserProjectsBy(userid);
+        List<UserProject> userProjects = getUserProjectsBy(userId);
 
         List<Long> projectIds = userProjects.stream()
                 .map(userProject -> userProject.getProject().getId())
