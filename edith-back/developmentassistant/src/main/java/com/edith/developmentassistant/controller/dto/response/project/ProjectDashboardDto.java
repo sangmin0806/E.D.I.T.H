59d463501c1c29859b410accdd39706deaa826bc
유재광 <marmong9770@gmail.com>
2024-11-15 14:42:03 +0900

    fix: class -> record 수정

--- edith-back/developmentassistant/src/main/java/com/edith/developmentassistant/controller/dto/response/project/ProjectDashboardDto.java
+++ edith-back/developmentassistant/src/main/java/com/edith/developmentassistant/controller/dto/response/project/ProjectDashboardDto.java
@@ -2,14 +2,14 @@ package com.edith.developmentassistant.controller.dto.response.project;
 
 import java.util.List;
 import lombok.Builder;
-import lombok.Getter;
 
 @Builder
-@Getter
-public class ProjectDashboardDto {
-    private String recentCommitMessage;
-    private String recentCodeReview;
-    private String advice;
-    private List<String> fixLogs;
-    private List<String> techStack;
+public record ProjectDashboardDto(
+        String recentCommitMessage,
+        String recentCodeReview,
+        String advice,
+        List<String> fixLogs,
+        List<String> techStack
+) {
+
 }
