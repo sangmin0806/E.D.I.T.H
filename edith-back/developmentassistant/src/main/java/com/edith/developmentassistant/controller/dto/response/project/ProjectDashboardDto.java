f388508ea022d41364d63cd573445b6a70b17251
유재광 <marmong9770@gmail.com>
2024-11-15 14:37:39 +0900

    fix: Json 직렬화 버그 픽스

--- edith-back/developmentassistant/src/main/java/com/edith/developmentassistant/controller/dto/response/project/ProjectDashboardDto.java
+++ edith-back/developmentassistant/src/main/java/com/edith/developmentassistant/controller/dto/response/project/ProjectDashboardDto.java
@@ -2,8 +2,10 @@ package com.edith.developmentassistant.controller.dto.response.project;
 
 import java.util.List;
 import lombok.Builder;
+import lombok.Getter;
 
 @Builder
+@Getter
 public class ProjectDashboardDto {
     private String recentCommitMessage;
     private String recentCodeReview;
