895016735da89023b4a467b257e1d9b0bca13ae7
유재광 <marmong9770@gmail.com>
2024-11-18 01:52:00 +0900

    fix: bug fix

--- edith-back/rag/flaskProject/app/services/codebert_model.py
+++ edith-back/rag/flaskProject/app/services/codebert_model.py
@@ -1,18 +1,19 @@
 # codebert_model.py
 from transformers import RobertaTokenizer, RobertaModel
 import torch
+from threading import Lock
 
-# 모델과 토크나이저를 None으로 초기화
 tokenizer = None
 model = None
+model_lock = Lock()
 
 def get_code_embedding(code_snippet):
     global tokenizer, model
 
-    # 모델과 토크나이저가 로드되지 않았다면 로드
-    if tokenizer is None or model is None:
-        tokenizer = RobertaTokenizer.from_pretrained('microsoft/graphcodebert-base')
-        model = RobertaModel.from_pretrained('microsoft/graphcodebert-base')
+    with model_lock:
+        if tokenizer is None or model is None:
+            tokenizer = RobertaTokenizer.from_pretrained('microsoft/graphcodebert-base')
+            model = RobertaModel.from_pretrained('microsoft/graphcodebert-base')
 
     inputs = tokenizer(code_snippet, return_tensors="pt", truncation=True, max_length=512)
     with torch.no_grad():
