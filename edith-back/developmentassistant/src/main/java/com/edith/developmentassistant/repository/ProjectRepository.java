38132005cae8fea27bb06241e058cc742ef8fabc
김민주 <mj1584.mk@gmail.com>
2024-11-18 17:21:56 +0900

    fix: gitgraph fetch error fix, 요청 시마다 프로젝트 토큰 생성 코드 삭제

--- edith-back/developmentassistant/src/main/java/com/edith/developmentassistant/repository/ProjectRepository.java
+++ edith-back/developmentassistant/src/main/java/com/edith/developmentassistant/repository/ProjectRepository.java
@@ -1,8 +1,11 @@
 package com.edith.developmentassistant.repository;
 
 import com.edith.developmentassistant.domain.Project;
+import com.edith.developmentassistant.domain.UserProject;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProjectRepository extends JpaRepository<Project, Long> {
 
+    Optional<Project> findById(Long projectId);
 }
