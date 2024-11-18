e05e48c43ec6467185f32743289557fed1940e7a
신민경 <tlsalsrud252@naver.com>
2024-11-18 16:16:38 +0900

    fix: projectName 데이터 넘기기

--- edith-front/src/store/repoPageStore.ts
+++ edith-front/src/store/repoPageStore.ts
@@ -4,8 +4,12 @@ interface ComponentState {
   showProject: number;
   showDashboard: boolean;
   selectedProjectID: number | undefined;
+  dashboardProjectName: string;
+  dashboardProjectContents: string;
   toggleComponent: (num: number) => void;
   togglePortfolio: () => void;
+  setDashboardProjectName: (projectName: string | null) => void;
+  setDashboardProjectContents: (projectContents: string | null) => void;
   setShowComponentOne: () => void;
   setShowDashboardTrue: () => void;
   setSelectedProjectID: (id: number | null) => void;
@@ -15,9 +19,15 @@ export const useComponentStore = create<ComponentState>((set) => ({
   showProject: 1,
   selectedProjectID: undefined,
   showDashboard: true,
+  dashboardProjectName: "",
+  dashboardProjectContents: "",
   toggleComponent: (num: number) => set(() => ({ showProject: num })),
   togglePortfolio: () =>
     set((state) => ({ showDashboard: !state.showDashboard })),
+  setDashboardProjectName: (projectName: string | null) =>
+    set(() => ({ dashboardProjectName: projectName ?? "" })),
+  setDashboardProjectContents: (projectContents: string | null) =>
+    set(() => ({ dashboardProjectContents: projectContents ?? "" })),
   setShowComponentOne: () => set(() => ({ showProject: 1 })),
   setShowDashboardTrue: () => set(() => ({ showDashboard: true })),
   setSelectedProjectID: (id) => set({ selectedProjectID: id ?? undefined }),
