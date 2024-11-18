73e6a4d260b17f298b3ccc5dbf3f28d53f939fcc
sangmin0806 <cjstkd2866@naver.com>
2024-11-19 00:00:11 +0900

    fix:test

--- edith-front/src/pages/common/FinishJoinPage.tsx
+++ edith-front/src/pages/common/FinishJoinPage.tsx
@@ -2,19 +2,12 @@ import mainLeft from "../../assets/main_left.png";
 import mainRight from "../../assets/main_right.png";
 import logo from "../../assets/logo.png";
 
-import { useNavigate } from "react-router-dom";
 import { motion } from "framer-motion";
 import { useRedirectIfLoggedIn } from "../../hooks/useAuth";
 
 function FinishJoinPage() {
-  const navigate = useNavigate();
-  const handleGoMain = () => {
-    navigate("/");
-  };
-  const handleRegisterFace = () => {
-    navigate("/register-face");
-  };
   useRedirectIfLoggedIn();
+
   return (
     <>
       <img
@@ -38,38 +31,16 @@ function FinishJoinPage() {
       >
         <div className="w-[100vw] h-[100vh] flex justify-center items-center">
           <div className="z-20 flex flex-col gap-6 items-center">
-            <div className="flex flex-col gap-4">
-              <p className="text-center text-black text-2xl font-semibold">
-                EDITH 회원가입이
-                <br />
-                성공적으로 완료되었습니다 !
-              </p>
-              <p className="text-center text-black text-2xl font-normal">
-                얼굴인식 로그인 등록도 진행하시겠습니까?
-              </p>
-            </div>
-            <div className="flex gap-4">
-              <div
-                onClick={handleRegisterFace}
-                className="p-2 bg-black rounded-2xl justify-center items-center gap-2.5 inline-flex cursor-pointer"
-              >
-                <p className="min-w-36 text-center text-white text-xl font-medium font-['Pretendard Variable']">
-                  얼굴 등록 진행
-                </p>
-              </div>
-              <div className="p-2 bg-white rounded-2xl border border-black justify-center items-center gap-2.5 inline-flex cursor-pointer">
-                <p
-                  className="text-center text-black text-xl font-medium font-['Pretendard Variable']"
-                  onClick={handleGoMain}
-                >
-                  아니요, 다음에 할게요
-                </p>
-              </div>
-            </div>
+            <p className="text-center text-black text-2xl font-semibold">
+              EDITH 회원가입이
+              <br />
+              성공적으로 완료되었습니다 !
+            </p>
           </div>
         </div>
       </motion.div>
     </>
   );
 }
+
 export default FinishJoinPage;
