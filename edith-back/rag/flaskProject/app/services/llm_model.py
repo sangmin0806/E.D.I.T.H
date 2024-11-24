51242d40153060a7a6f65bb1dcea8e08e75aebdc
Lee-JoungHyun <doublehyun98@gmail.com>
2024-11-24 15:16:32 +0900

    fix: key 분리

--- edith-back/rag/flaskProject/app/services/llm_model.py
+++ edith-back/rag/flaskProject/app/services/llm_model.py
@@ -5,7 +5,10 @@ import time
 class LLMModel:
     def __init__(self, model='gpt-4o-mini'):
         # 키를 배열로 로드
-        self.api_keys = os.getenv('OPENAI_API_KEY').split(',')
+        self.api_keys = [
+            os.getenv('OPENAI_API_KEY1'),
+            os.getenv('OPENAI_API_KEY2')
+        ]
         self.current_key_index = 0
         self.model = model
         self.temperature = 0
@@ -18,22 +21,21 @@ class LLMModel:
             openai_api_key=api_key.strip()
         )
 
-    def _rotate_key(self):
+    def rotate_key(self):
         self.current_key_index = (self.current_key_index + 1) % len(self.api_keys)
         new_key = self.api_keys[self.current_key_index].strip()
-        print(f"Switching to API key: {new_key[:10]}...")  # 일부만 출력해 보안 유지
         self.llm = self._initialize_llm(new_key)
 
-    def request(self, **kwargs):
-        while True:
-            try:
-                # LLM 호출
-                return self.llm(**kwargs)
-            except Exception as e:
-                error_message = str(e)
-                if "rate limit" in error_message or "TPM" in error_message:
-                    print(f"Rate limit reached for key: {self.api_keys[self.current_key_index][:10]}...")
-                    self._rotate_key()
-                    time.sleep(2)  # 키 교체 후 잠시 대기
-                else:
-                    raise e
+    # def request(self, **kwargs):
+    #     while True:
+    #         try:
+    #             # LLM 호출
+    #             return self.llm(**kwargs)
+    #         except Exception as e:
+    #             error_message = str(e)
+    #             if "rate limit" in error_message or "TPM" in error_message:
+    #                 print(f"Rate limit reached for key: {self.api_keys[self.current_key_index][:10]}...")
+    #                 self._rotate_key()
+    #                 time.sleep(2)  # 키 교체 후 잠시 대기
+    #             else:
+    #                 raise e
